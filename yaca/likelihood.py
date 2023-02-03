767ce4e9f55a0d9b80dfa5167369bf91dd45d9a8
Gertjan Bisschop <gertjanbisschop@gmail.com>
2023-02-03 21:25:30 +0000

    first outline likelihood

--- yaca/likelihood.py
+++ yaca/likelihood.py
@@ -9,7 +9,7 @@ from yaca import sim
 
 def decap_to_overlap(ts, node):
     overlap = dict()
-    children_pairs = []
+    children_pairs = set()
 
     for tree in ts.trees():
         all_lineages = []
@@ -18,7 +18,7 @@ def decap_to_overlap(ts, node):
                 root = tree.children(root)[0]
             all_lineages.append(root)
             if root == node:
-                children_pairs.append(tree.children(root))
+                children_pairs.add(tree.children(root))
         all_lineages.sort()
         for pair in itertools.combinations(all_lineages, 2):
             if pair not in overlap:
@@ -28,16 +28,34 @@ def decap_to_overlap(ts, node):
     return overlap, children_pairs
 
 
-def single_step_prob(pairwise_overlap, pairs, last_event, t):
+def rate(time, T, overlap):
+    return 1 + (2 * time + T) * overlap
+
+
+def rate_integral(time, T, overlap):
+    t2 = overlap
+    t1 = overlap * T + 1
+    return time * (t2 * time + t1)
+
+
+def single_step_prob(pairwise_overlap, pairs, last_event, t, rho):
     single_event = 0
+    mean_intensity = 0
+    pairwise_overlap = {k: v * rho / 2 for k, v in pairwise_overlap.items()}
+
     for pair in pairs:
-        single_event += sim.coal_rate(1, rho, time, last_event, 1)
-    mean_intensity = rate_integral(time, rho, overlap)
-    p_next_event = np.exp(-np.sum(mean_intensity_all))
-    return single_event * p_next_event
+        T = 2 * last_event - sum(node_times[i] for i in pair)
+        p_single_event += rate(time, T, pairwise_overlap[pair])
+
+    for pair, overlap in pairwise_overlap.items():
+        overlap = pairwise_overlap[pair]
+        T = 2 * last_event - sum(node_times[i] for i in pair)
+        mean_intensity += rate_integral(time, T, overlap)
+
+    return p_single_event * np.exp(-mean_intensity)
 
 
-def get_p(ts):
+def get_p(ts, rho):
     num_nodes = ts.num_nodes
     num_samples = ts.num_samples
     sequence_length = ts.sequence_length
@@ -54,7 +72,7 @@ def get_p(ts):
         # nodes[i] is registers the next coalescence event back in time
         # we need a list of all pairs of children of nodes[i]
         decap = ts.decapitate(t)
-        updated_pairwise_overlap, pairs = decap_to_overlap(decap, tables.nodes[i])
+        updated_pairwise_overlap, pairs = decap_to_overlap(decap, tables.nodes[i], rho)
         p *= single_step_prob(pairwise_overlap, pairs, last_event, t)
         pairwise_overlap = updated_pairwise_overlap.copy()
         last_event = t
